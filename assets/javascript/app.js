e9702019406cd11834a0b831ce63d534a41956ea
aabbatiello <anthonyjabbatiello@gmail.com>
2017-05-03 21:49:54 -0400

    most updated hw still trying to figure out event listener for still/animate

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -33,7 +33,7 @@ $(document).ready(function() {
         // This line grabs the input from the textbox
         var topic = $("#gangsta-input").val().trim();
 
-        // Adding movie from the textbox to our array
+        // Adding Gangsta from the textbox to our array
         topics.push(topic);
 
         // Calling renderButtons which handles the processing of our topics array
@@ -43,13 +43,13 @@ $(document).ready(function() {
 
 
 
-// fetch gangsta Gifs with the Giphy API
+// gets gangsta Gifs with the Giphy API
 function Gangsta() {
   // Get the actors name from the button clicked
         var gangstaname = $(this).attr("data-name");
 
   //GIPHY URL
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gangstaname + 
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gangstaname + 
                  "&api_key=dc6zaTOxFJmzC&limit=10";
                  
 // Make the AJAX call to the Giphy API
@@ -57,22 +57,38 @@ $.ajax({
           url: queryURL,
           method: "GET"
         })
+
+  // After the data comes back from the API
         .done(function(response){
-          var results = response.data;
 
+    // Storing an array of results in the results variable
+        var results = response.data;
+
+  // Looping over every result item
           for (var i = 0; i < results.length; i++) {
+   
+   // Creating a div with the class "item"
             var gifDiv = $("<div class='item'>");
+            gifDiv.addClass("GangstaGif");
 
+     // Storing the result item's rating
             var rating = results[i].rating;
 
+    // Creating a paragraph tag with the result item's rating
             var p = $("<p>").text("Rating: " + rating);
 
+
+      // Creating an image tag
             var personImage = $("<img>");
-            personImage.attr("src", results[i].images.fixed_height_still.url);
+
+       // Giving the image tag an src attribute of a proprty pulled off the
+      // result item
+            personImage.attr("src", results[i].images.fixed_height.url);
 
 
-            gifDiv.prepend(p);
-            gifDiv.prepend(personImage);
+        // Appending the paragraph and personImage we created to the "gifDiv" div we created
+            gifDiv.append(p);
+            gifDiv.append(personImage);
 
             $("#gifs-appear-here").prepend(gifDiv);
           }
@@ -80,3 +96,29 @@ $.ajax({
       };
 
 $(document).on("click", ".gangstabutton", Gangsta);
+
+//function GangstaAnimate() {
+  //    if (state === "still") {
+    //    $(this).attr("src", $(this).attr("data-animate"));
+      //  $(this).attr("data-state", "animate");
+      //} else {
+       // $(this).attr("src", $(this).attr("data-still"));
+       // $(this).attr("data-state", "still");
+    //  }
+    //};
+
+
+  //$(document).on("click", "GangstaGif", GangstaAnimate);
+
+
+
+
+
+
+
+
+
+
+
+
+
